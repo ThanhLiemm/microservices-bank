a5ee390d56bff6dcab38fce9b357a68432a507c3
thanhliem <thanhliem4121999@gmail.com>
2024-04-14 23:16:02 +0700

    feat(accounts): create entity dto mapper

---
+++ accounts/src/main/java/com/thanhliem/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,28 @@
+package com.thanhliem.accounts.mapper;
+
+import com.thanhliem.accounts.dto.AccountsDto;
+import com.thanhliem.accounts.entity.Accounts;
+
+public class AccountsMapper {
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts) {
+        return mapToAccountsDto(accounts, new AccountsDto());
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountDto) {
+        return mapToAccounts(accountDto, new Accounts());
+    }
+}
---
+++ accounts/src/main/java/com/thanhliem/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,28 @@
+package com.thanhliem.accounts.mapper;
+
+import com.thanhliem.accounts.dto.CustomerDto;
+import com.thanhliem.accounts.entity.Customer;
+
+public class CustomerMapper {
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static CustomerDto mapToCustomerDto(Customer customer) {
+        return mapToCustomerDto(customer, new CustomerDto());
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto) {
+        return mapToCustomer(customerDto, new Customer());
+    }
+}
