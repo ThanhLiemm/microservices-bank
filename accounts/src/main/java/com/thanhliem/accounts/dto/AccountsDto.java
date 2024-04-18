1611a9b388048008b49448398eb4a0e609e4070d
thanhliem <thanhliem4121999@gmail.com>
2024-04-18 17:47:29 +0700

    feat(accounts): add spring doc to accounts service

--- accounts/src/main/java/com/thanhliem/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/thanhliem/accounts/dto/AccountsDto.java
@@ -1,18 +1,32 @@
 package com.thanhliem.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 @Data
 public class AccountsDto {
     @NotEmpty(message = "AccountNumber can not be a null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
+    @Schema(
+            description = "Account Number of TL Bank account", example = "0123456789"
+    )
     private Long accountNumber;
 
+    @Schema(
+            description = "Account type of TL Bank account", example = "Savings"
+    )
     @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
 
+    @Schema(
+            description = "TL Bank branch address", example = "123 NewYork"
+    )
     @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 }
