6aee5bc40a0c56b84cc2abcd10d1488aba301949
thanhliem <thanhliem4121999@gmail.com>
2024-04-28 18:51:57 +0700

    feat(accounts, cards, loans): connect to config server

--- loans/src/main/java/com/thanhliem/loans/LoansApplication.java
+++ loans/src/main/java/com/thanhliem/loans/LoansApplication.java
@@ -1,5 +1,6 @@
 package com.thanhliem.loans;
 
+import com.thanhliem.loans.dto.LoansContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @OpenAPIDefinition(
@@ -29,6 +31,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
                 url = ""
         )
 )
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoansApplication {
