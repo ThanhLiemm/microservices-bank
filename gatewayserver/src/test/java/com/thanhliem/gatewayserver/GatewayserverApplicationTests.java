b34b8246c0db67ec1f716c340cc02f6a0fe19f76
thanhliem <thanhliem4121999@gmail.com>
2024-05-14 22:12:47 +0700

    feat(gateway): add gateway server to handle all external request

--- gatewayserver/src/test/java/com/thanhliem/gatewayserver/GatewayserverApplicationTests.java
+++ gatewayserver/src/test/java/com/thanhliem/gatewayserver/GatewayserverApplicationTests.java
@@ -1,10 +1,10 @@
-package com.thanhliem.eurekaserver;
+package com.thanhliem.gatewayserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaserverApplicationTests {
+class GatewayserverApplicationTests {
 
 	@Test
 	void contextLoads() {
