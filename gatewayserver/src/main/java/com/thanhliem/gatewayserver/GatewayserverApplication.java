00f206769ba7496161e6366e973d0a1610a27222
thanhliem <thanhliem4121999@gmail.com>
2024-07-25 22:39:38 +0700

    feat(gateway): implement retry pattern in gateway

--- gatewayserver/src/main/java/com/thanhliem/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/thanhliem/gatewayserver/GatewayserverApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -27,7 +29,10 @@ public class GatewayserverApplication {
 				.route(p -> p
 						.path("/tl_bank/loans/**")
 						.filters( f -> f.rewritePath("/tl_bank/loans/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.retry(retryConfig -> retryConfig.setRetries(2)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(500), 2,true)))
 						.uri("lb://LOANS"))
 				.route(p -> p
 						.path("/tl_bank/cards/**")
