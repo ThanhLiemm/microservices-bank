b88699143acbcbd996c9418c9707ece907016030
thanhliem <thanhliem4121999@gmail.com>
2024-07-27 19:11:26 +0700

    feat(accounts): implement ratelimiter pattern in accounts

--- accounts/src/main/java/com/thanhliem/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/thanhliem/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.thanhliem.accounts.dto.ErrorResponseDto;
 import com.thanhliem.accounts.dto.ResponseDto;
 import com.thanhliem.accounts.service.IAccountsService;
 import com.thanhliem.accounts.utils.constants.AccountsConstants;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -224,11 +225,19 @@ public class AccountsController {
             )
     }
     )
+
+    @RateLimiter(name= "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
